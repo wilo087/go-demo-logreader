58ddc91d96b0af439e675ee83c106e1b016600ab
Wilowayne De La Cruz G <wilo0087@gmail.com>
2019-10-12 16:15:16 -0400

    Function file reader has done

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"os"
+	"log"
+    "bufio"
+    "strings"
+    "fmt"
+)
+
 func main()  {
-  
+	f, err := os.Open("mylog.log")
+	if err != nil {
+		log.Fatal(err)		
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	
+	for {
+		s, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+		if strings.Contains(s, "INFO") {
+            fmt.Println(s)
+        }
+	}
 }
